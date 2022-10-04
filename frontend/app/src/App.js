9eda1c26ce7aa964ea86414c4d6a3a738ba71a8d
Kővári Bencem <kovaribence95@gmail.com>
2022-10-04 15:38:43 +0200

    create add button to TODO list (start fetch)

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 
 function App() {
     const [todos, setTodos] = useState([])
+    const [input, setInput] = useState('')
 
     const fetchTodos = () => {
         fetch('/todos')
@@ -10,13 +11,44 @@ function App() {
             .then((data) => setTodos(data))
     }
 
+    const addTodo = (id) => {
+        const bodyObject = {
+            id: id,
+            desc: input,
+        }
+
+        console.log(bodyObject)
+
+        fetch('/addTodo', {
+            method: 'POST',
+            body: JSON.stringify(bodyObject),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+    }
+
     useEffect(fetchTodos, [])
 
     return (
         <div className="App">
-            {todos.map((todo) => (
-                <h2>{todo.desc}</h2>
+            {todos.map((todo, index) => (
+                <h2 key={index}>
+                    id:{todo.id}
+                    <br />
+                    {todo.desc}
+                </h2>
             ))}
+
+            <input
+                type="text"
+                value={input}
+                placeholder="enter description"
+                onChange={(evt) => setInput(evt.target.value)}
+            />
+            <button onClick={() => addTodo(todos[todos.length - 1].id + 1)}>add Todo</button>
         </div>
     )
 }
